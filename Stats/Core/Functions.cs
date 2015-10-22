3d0c62aa86a33ffe30c85e3cd6e522bca91f3591
dcpulido <dcpulido91@gmail.com>
2015-10-22 17:37:24 +0200

    a ver ahora

--- Stats/Core/Functions.cs
+++ Stats/Core/Functions.cs
@@ -1,11 +1,34 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace Stats
 {
-	public class Functions
+	public class Functions:Fachada
 	{
-		public Functions ()
-		{
+		private List<Session> lst;
+		private Reader red;
+		private String rout;
+		public Functions (String route){
+			rout = route;
+			red = new Reader (rout);
+			lst = red.getFile ();
+		}
+		public String getVelMedTot(){
+			
+		}
+
+		String getVelMedDay(String day);
+		String getNumStpsTot();
+		String getNumStpsDay(String day);
+		String getNumHourTot();
+		String getNumHourDay(String day);
+		private int getDurTot(List<Session> lt){
+			int toret;
+			foreach(Session k in lt){
+				toret+= k.duration;
+			}
+			return toret;
 		}
 	}
 }
